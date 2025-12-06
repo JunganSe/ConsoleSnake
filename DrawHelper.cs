00be84143c56a5cff02a4a85ec0ed61044326ca0
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-12-06 22:51:28 +0100

    Adjust namespace and cleanup usings

--- DrawHelper.cs
+++ DrawHelper.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Snake
+namespace ConsoleSnake
 {
     class DrawHelper
     {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading;
-
-namespace Snake
+﻿namespace ConsoleSnake
 {
     class Program
     {
